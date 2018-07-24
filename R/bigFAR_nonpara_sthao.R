d392c23abe22ad40909b18527f6988713cd6aab5
sthao <sthao@lsce.ipsl.fr>
2018-07-24 14:05:15 +0200

    fix for vcov.farrfit_emp and vcov.farrfit_exp

--- R/bigFAR_nonpara_sthao.R
+++ R/bigFAR_nonpara_sthao.R
@@ -9,27 +9,27 @@ estim_FAR.emp <- function(x, z, u){
 #' Empirical estimation of the FAR from bootstrap samples
 #'
 #' \code{boot_FAR_fit.emp} returns an object of class \code{("boot_FARfit.emp", "boot_FARfit", "FARfit")}
-#' which contains the estimations of the FAR for each bootstrap sample and
+#' which contains the estimates of the FAR for each bootstrap sample and
 #' for different return periods \code{rp}
-#' 
-#' This function returns bootstrap empirical estimations of FAR, the fraction of attributable risk
+#'
+#' This function returns bootstrap empirical estimates of FAR, the fraction of attributable risk
 #' where events are defined in terms of a threshold exceendance.
 #' The FAR is estimate from the bootstrap samples of the data x and z
-#' that are obtained by resampling bootstrap. 
+#' that are obtained by resampling bootstrap.
 #' The first bootstrap sample is obtained from the original dataset of x and y.
-#' 
+#'
 #' @param x the variable of interest in the counterfactual world.
 #' @param z the variable of interest in the factual world.
 #' @param u the thresholds used to define the events.
 #' @param B the number of bootstrap samples to draw.
-#' 
+#'
 #' @return An object of class \code{("boot_FARfit.emp", "boot_FARfit", "FARfit")}.
 #' It is a matrix where each columm contains the FAR estimated for the returns periods \code{rp}
 #' for a given bootstrap sample.
-#' 
+#'
 #' @examples
 #'  library(evd)
-#'  
+#'
 #'  muF <-  1; xiF <- .15; sigmaF <-  1.412538 #  cst^(-xiF) # .05^(-xi1);
 #'  # asymptotic limit for the far in this case with a Frechet distributiom
 #'  boundFrechet <- 1 - sigmaF^(-1/xiF)
@@ -39,7 +39,7 @@ estim_FAR.emp <- function(x, z, u){
 #'  set.seed(4)
 #'  z = rgev(size, loc = (sigmaF), scale = xiF * sigmaF, shape = xiF)
 #'  x = rgev(length(z), loc=(1), scale = xiF, shape=xiF)
-#'  
+#'
 #'  rp = seq(from = 2, to = 30, length = 200)
 #'  u  = qgev(1 - (1 / rp),loc = muF, scale = xiF, shape = xiF)
 #'
@@ -47,7 +47,7 @@ estim_FAR.emp <- function(x, z, u){
 #'  boot_FAR.emp <- boot_FAR_fit.emp(x = x, z = z, u = u, B = 10)
 #'  print(boot_FAR.emp)
 #'  confint(boot_FAR.emp)
-#'  
+#'
 #'  ylim <- range(boundFrechet, boot_FAR.emp)
 #'  plot(boot_FAR.emp, ylim = ylim, main = "boot FAR empirical")
 #'  # Theoretical FAR for in this case (Z = sigmaF * X  with X ~ Frechet)
@@ -71,33 +71,33 @@ boot_FAR_fit.emp <- function(x, z , u, B = 100){
 #' Compute confidence intervals from bootstrap samples of the FAR
 #'
 #' \code{confint.boot_FARfit} returns an matrix that contains the confidence interval
-#' for the far computed empirically from the bootstrap estimations of the far
+#' for the far computed empirically from the bootstrap estimates of the far
 #' for different exceedance thresholds \code{u}
-#' 
+#'
 #' This function returns a two-column matrix that contains the confidence intervals
-#' for the FAR computed empirically from the bootstrap estimations of the FAR.
-#' 
+#' for the FAR computed empirically from the bootstrap estimates of the FAR.
+#'
 #' @param object an object with the class \code{boot_FARfit}. It is a matrix containing
-#' the bootstrap samples of the far. Each line of this matrix represent the estimated far for 
+#' the bootstrap samples of the far. Each line of this matrix represent the estimated far for
 #' different thresholds \code{u} and colum corresponds to a differents bootstrap samples of the
 #' data.
 #' @param parm a vector of thresholds for which to compute the confidence interval for the FAR.
 #' The thresholds have to be selected from the one present in \code{rownames(object)}.
-#' 
-#' @param level a numerical value between 0 and 1 corresponding to the confidence level 
+#'
+#' @param level a numerical value between 0 and 1 corresponding to the confidence level
 #' of the confidence intervals.
-#' 
+#'
 #' @param ... not used.
-#' 
+#'
 #' @return A two-column matrix that contains the confidence interval
-#' for the FAR computed empirically from the bootstrap estimations of the FAR.
+#' for the FAR computed empirically from the bootstrap estimates of the FAR.
 #' The first colum is for the lower bound of the confidence interval and the second one
-#' for the upper bound. Each line of the matrix represents the condidence interval for a 
+#' for the upper bound. Each line of the matrix represents the condidence interval for a
 #' different threshold \code{u}
-#' 
+#'
 #' @examples
 #'  library(evd)
-#'  
+#'
 #'  muF <-  1; xiF <- .15; sigmaF <-  1.412538 #  cst^(-xiF) # .05^(-xi1);
 #'  # asymptotic limit for the far in this case with a Frechet distributiom
 #'  boundFrechet <- frechet_lim(sigma = sigmaF, xi = xiF)
@@ -107,7 +107,7 @@ boot_FAR_fit.emp <- function(x, z , u, B = 100){
 #'  set.seed(4)
 #'  z = rgev(size, loc = (sigmaF), scale = xiF * sigmaF, shape = xiF)
 #'  x = rgev(length(z), loc=(1), scale = xiF, shape=xiF)
-#'  
+#'
 #'  rp = seq(from = 2, to = 30, length = 200)
 #'  u  = qgev(1 - (1 / rp),loc = muF, scale = xiF, shape = xiF)
 #'
@@ -115,7 +115,7 @@ boot_FAR_fit.emp <- function(x, z , u, B = 100){
 #'  boot_FAR.emp <- boot_FAR_fit.emp(x = x, z = z, u = u, B = 10)
 #'  print(boot_FAR.emp)
 #'  confint(boot_FAR.emp)
-#'  
+#'
 #'  ylim <- range(boundFrechet, boot_FAR.emp)
 #'  plot(boot_FAR.emp, ylim = ylim, main = "boot FAR empirical")
 #'  # Theoretical FAR for in this case (Z = sigmaF * X  with X ~ Frechet)
@@ -129,10 +129,10 @@ confint.boot_FARfit <- function(object, parm = dimnames(object)$u, level = 0.95,
   return(ic)
 }
 
-#' @rdname boot_FAR_fit.emp 
+#' @rdname boot_FAR_fit.emp
 #' @export
 print.FARfit <- function(x, ...){
-  FARdf <- data.frame(u = get_u(x), 
+  FARdf <- data.frame(u = get_u(x),
                       FAR_hat = get_FAR(x),
                       sigma_FAR_hat = get_sigma_FAR(x))
   rownames(FARdf) <- NULL
@@ -142,10 +142,10 @@ print.FARfit <- function(x, ...){
 
 
 #' @param ... additional arguments for the plot.
-#' @rdname boot_FAR_fit.emp 
+#' @rdname boot_FAR_fit.emp
 #' @export
 plot.FARfit <-function(x, ...){
-  FAR <- get_FAR(x) 
+  FAR <- get_FAR(x)
   u <- get_u(x)
   #
   # quantile for two side 90 confidence interval
@@ -153,8 +153,8 @@ plot.FARfit <-function(x, ...){
   ci90 <- confint(x, names(FAR), level = 0.90)
   ellipsis <- list(...)
   if(is.null(ellipsis$ylim)){
-    ymin <- min(FAR,  ci90)
-    ymax <- max(FAR,  ci90)
+    ymin <- min(FAR,  ci90, na.rm = TRUE)
+    ymax <- max(FAR,  ci90, na.rm = TRUE)
     ylim <- c(ymin, ymax)
   } else {
     ylim = ellipsis$ylim
@@ -176,18 +176,21 @@ plot.FARfit <-function(x, ...){
   args_plot <- c(args_plot, ellipsis)
   do.call(plot, args_plot)
   grid(lwd = 3)
-  args_polygon <- list(x = c(u, u[lu:1]), 
+  args_polygon <- list(x = c(u, u[lu:1]),
                        y = c(ci90[, 1], ci90[lu:1, 2]),
                        col = col, border=F)
   args_polygon <- c(args_polygon, ellipsis)
   do.call(polygon, args_polygon)
-  lines(u, FAR, col="black", lwd = 1, lty = 1)
+  args_lines <- list(x =  rp,
+                     y = farr,
+                     col = "black")
+  args_lines <- c(args_lines, ellipsis)
+  do.call(lines, args_lines)
   # title("Exponential FAR(r)")
-  invisible(NULL)
 }
 
 get_FAR.boot_FARfit <- function(object, ...){
-  FAR <- apply(object, 1, mean) 
+  FAR <- apply(object, 1, mean)
 }
 
 get_u.boot_FARfit <- function(object, ...){
@@ -195,7 +198,7 @@ get_u.boot_FARfit <- function(object, ...){
 }
 
 get_sigma_FAR.boot_FARfit <- function(object, ...){
-  FAR <- apply(object, 1, sd) 
+  FAR <- apply(object, 1, sd)
 }
 
 get_FAR <- function(object, ...){
--- R/limfarFAR_gev_sthao.R
+++ R/limfarFAR_gev_sthao.R
@@ -1,9 +1,9 @@
 #' Theoretical far and FAR for three types of changes in a GEV distribution
 #'
 #' A set of functions to return the theoretical far and the theorecial FAR in 3 specific cases when X and Z
-#' follow a GEV distribution. 
-#' 
-#' These functions (\code{gumble_farr}, \code{frechet_farr}, \code{weibull_farr}, 
+#' follow a GEV distribution.
+#'
+#' These functions (\code{gumble_farr}, \code{frechet_farr}, \code{weibull_farr},
 #' \code{gumble_FAR}, \code{frechet_FAR}, \code{weibull_FAR})
 #' return the theoretical far and the FAR in 3 specific cases when X and Z
 #' follow a GEV distribution. The three specific cases described in Naveau et al. (2018) are :
@@ -15,33 +15,33 @@
 #'   \item X ~ Weibull with cdf G(x) = exp[-max(0, 1 + xi * x)^(-1 / xi)]
 #'    with x real, mu >= 0  and Z = mu + sigma * X
 #' }
-#' For these three cases, the limits of the far and the FAR coincide when the return period \code{r} grows to 
+#' For these three cases, the limits of the far and the FAR coincide when the return period \code{r} grows to
 #' infinity and when the threshold \code{u} move towards the upper support bound of the distribution.
 #' The functions \code{gumble_lim}, \code{frechet_lim} and \code{weibull_lim} compute those limits.
-#' 
+#'
 #' For the full reference, see : \emph{Naveau, P., Ribes, A., Zwiers, F., Hannart, A., Tuel, A., & Yiou, P.
 #' Revising return periods for record events in a climate event attribution context.
 #' J. Clim., 2018.}, \url{https://doi.org/10.1175/JCLI-D-16-0752.1}
-#' 
+#'
 #' @param r the return periods for which the far is to be estimated.
 #' @param u the thresholds for which the FAR is to be estimated.
 #' @param mu the shift in the GEV location parameter between X and Z
 #' @param sigma the multiplying factor in the GEV scale parameter between X and Z
 #' @param xi the value of th GEV shape parameter
-#' 
+#'
 #' @examples
 #'  library(evd)
 #'  # sample size
 #'  # size <- 100
-#'  
-#'  # Gumbel 
-#'  # Z = muDelta + X 
+#'
+#'  # Gumbel
+#'  # Z = muDelta + X
 #'  # x = rgev(size, loc = 0, scale = 1, shape = 0)
 #'  # z = rgev(size, loc = muDelta, scale = 1, shape = 0)
 #'  muG <-  0; sigmaG <-  1; xiG = 0 ; muDelta = 0.5
 #'  r = seq(from = 2, to = 100, length = 100)
 #'  u  = qgev(1 - (1 / r), loc = muDelta, scale = 1, shape = 0)
-#' 
+#'
 #'  # asymptotic limit for the far and the FAR in this case with a Gumble distributiom
 #'  glim <- gumble_lim(mu  = muDelta)
 #'  # theoretical far in this case with a Frechet distributiom
@@ -54,15 +54,15 @@
 #'  abline(h = glim)
 #'  plot(u, gFAR, ylim = ylim, main = "FAR theoretical value")
 #'  abline(h = glim)
-#' 
-#'  # Frechet 
-#'  # Z = sigmaDelta * X 
+#'
+#'  # Frechet
+#'  # Z = sigmaDelta * X
 #'  # x = rgev(size, loc = 1, scale = xiF, shape = xiF)
 #'  # z = rgev(size, loc = sigmaDelta, scale = xiF * sigmaDelta, shape = xiF)
 #'  muF <-  1; sigmaF <- xiF <- .15 ; sigmaDelta <-  1.412538
 #'  r = seq(from = 2, to = 100, length = 100)
 #'  u  = qgev(1 - (1 / r), loc = muF, scale = sigmaF, shape = xiF)
-#' 
+#'
 #'  # asymptotic limit for the far and the FAR in this case with a Frechet distributiom
 #'  flim <- frechet_lim(sigma = sigmaDelta, xi = xiF)
 #'  # theoretical far in this case with a Frechet distributiom
@@ -75,16 +75,16 @@
 #'  abline(h = flim)
 #'  plot(u, fFAR, ylim = ylim, main = "FAR theoretical value")
 #'  abline(h = flim)
-#'  
-#'  # Weibull 
-#'  # Z = muDelta + sigmaDelta * X 
-#'  # x = rgev(size, loc = 0, scale = 1, shape = xiF)
+#'
+#'  # Weibull
+#'  # Z = muDelta + sigmaDelta * X
+#'  # x = rgev(size, loc = 0, scale = 1, shape = xiW)
 #'  # z = rgev(size, loc = muDelta, scale =  sigmaDelta, shape = xiW)
 #'  muW <-  0 ; sigmaW <- 1 ; xiW <- -0.05;
 #'  sigmaDelta <-  1.1 ; muDelta <- (1 - sigmaDelta) / (-xiW)
 #'  r = seq(from = 2, to = 100, length = 100)
 #'  u  = qgev(1 - (1 / r), loc = muW, scale = sigmaW, shape = xiW)
-#' 
+#'
 #'  # asymptotic limit for the far and the FAR in this case with a Weibull distributiom
 #'  wlim <- weibull_lim(sigma = sigmaDelta, xi = xiW)
 #'  # theoretical far in this case with a Frechet distributiom
@@ -168,10 +168,10 @@ weibull_lim <- function(sigma, xi){
 gevfar=function(r, mu, sigma, xi){
   if(xi==0) {
     cst= exp(-mu)
-  } 
+  }
   if(xi!=0) {
     cst=(sigma)^(-1/xi)
-  } 
+  }
   out=rep(NA,length(r))
   for(i in 1:length(r)){
     out[i]=1-((1+cst*(r[i]-1))/r[i])
@@ -189,16 +189,16 @@ gevFAR=function(u, mu, sigma, xi){
   if(xi==0) {
     top=1-exp(-exp(-u))
     bottom=1-exp(-exp(-(u-mu)))
-  } 
+  }
   if(xi>0) {
     top=1-exp(-(u)^(-1/xi))
     bottom=1-exp(-(u/sigma)^(-1/xi))
-  }  
+  }
   if(xi<0) {
     top=1-exp(-(1+xi*u)^(-1/xi))
     cc=(sigma)^(1/xi)
     bottom=1-exp(-cc*(1+xi*u)^(-1/xi))
-  } 
+  }
   out=1-(top/bottom)
   return(out)
 }
